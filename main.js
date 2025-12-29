49b08529f5aa00f99164a4d4d76f733fed70c76c
infovsetngo-hub <info.vsetngo@gmail.com>
2025-12-29 22:26:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+// Hero Slider
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlides() {
+  slides.forEach((slide) => (slide.style.display = 'none'));
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = 'flex';
+  setTimeout(showSlides, 5000);
+}
+showSlides();
